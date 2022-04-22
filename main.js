9bf77e26b2ba198b386498b4b247bc9b3302455d
VitorCunhaP <vitorlostada@hotmail.com>
2022-04-21 22:27:19 -0300

    electron

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preloader.js"),
+    },
+  });
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <span id="node-version"></span>
+      <br />
+      <span id="chrome-version"></span>
+      <br />
+      <span id="electron-version"></span>
+    </div>
+  );
+}
